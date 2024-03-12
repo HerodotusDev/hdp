0d1cc1e8341b23e6a09ec8397055161d1cec0682
Pia <gayeongparkk@gmail.com>
2024-03-12 13:19:50 +0900

    cleanup unwrap

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{bail, Result};
 use std::{sync::Arc, vec};
 
 use clap::{Parser, Subcommand};
@@ -101,12 +102,12 @@ async fn handle_run(
     rpc_url: Option<String>,
     output_file: Option<String>,
     cairo_input: Option<String>,
-) {
+) -> Result<()> {
     let start_run = std::time::Instant::now();
     let config = Config::init(rpc_url, datalakes, tasks).await;
     let abstract_fetcher = AbstractFetcher::new(config.rpc_url.clone());
-    let tasks = tasks_decoder(config.tasks.clone()).unwrap();
-    let datalakes = datalakes_decoder(config.datalakes.clone()).unwrap();
+    let tasks = tasks_decoder(config.tasks.clone())?;
+    let datalakes = datalakes_decoder(config.datalakes.clone())?;
 
     println!("tasks: \n{:?}\n", tasks);
     println!("datalakes: \n{:?}\n", datalakes);
@@ -115,27 +116,37 @@ async fn handle_run(
         panic!("Tasks and datalakes must have the same length");
     }
 
-    let res = evaluator(
+    match evaluator(
         tasks,
         Some(datalakes),
         Arc::new(RwLock::new(abstract_fetcher)),
     )
     .await
-    .unwrap();
+    {
+        Ok(res) => {
+            println!("Result: \n{:?}\n", res);
+            let duration_run = start_run.elapsed();
+            println!("Time elapsed in run evaluator is: {:?}", duration_run);
 
-    let duration_run = start_run.elapsed();
-    println!("Time elapsed in run evaluator is: {:?}", duration_run);
+            if let Some(output_file) = output_file {
+                res.save_to_file(&output_file, false)?;
+            }
+            if let Some(cairo_input) = cairo_input {
+                res.save_to_file(&cairo_input, true)?;
+            }
 
-    if let Some(output_file) = output_file {
-        res.save_to_file(&output_file, false).unwrap();
-    }
-    if let Some(cairo_input) = cairo_input {
-        res.save_to_file(&cairo_input, true).unwrap();
+            Ok(())
+        }
+        Err(e) => {
+            let duration_run = start_run.elapsed();
+            println!("Time elapsed in run evaluator is: {:?}", duration_run);
+            bail!(e);
+        }
     }
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
     let cli = Cli::parse();
     dotenv::dotenv().ok();
     match cli.command {
@@ -166,12 +177,12 @@ async fn main() {
                 }
             };
             println!("Original datalake: \n{:?}\n", datalake);
-            let encoded_datalake = datalakes_encoder(vec![datalake]).unwrap();
+            let encoded_datalake = datalakes_encoder(vec![datalake])?;
             println!("Encoded datalake: \n{}\n", encoded_datalake);
             let tasks =
                 common::task::ComputationalTask::new(None, aggregate_fn_id, aggregate_fn_ctx);
             println!("Original task: \n{:?}\n", tasks);
-            let encoded_task = tasks_encoder(vec![tasks]).unwrap();
+            let encoded_task = tasks_encoder(vec![tasks])?;
             println!("Encoded task: \n{}\n", encoded_task);
 
             // if allow_run is true, then run the evaluator
@@ -183,26 +194,31 @@ async fn main() {
                     output_file,
                     cairo_input,
                 )
-                .await;
+                .await
+            } else {
+                Ok(())
             }
         }
         Commands::Decode { tasks, datalakes } => {
-            let datalakes = datalakes_decoder(datalakes.clone()).unwrap();
+            let datalakes = datalakes_decoder(datalakes.clone())?;
             println!("datalakes: \n{:?}\n", datalakes);
 
-            let tasks = tasks_decoder(tasks).unwrap();
+            let tasks = tasks_decoder(tasks)?;
             println!("tasks: \n{:?}\n", tasks);
 
             if tasks.len() != datalakes.len() {
-                panic!("Tasks and datalakes must have the same length");
+                bail!("Tasks and datalakes must have the same length");
+            } else {
+                Ok(())
             }
         }
         Commands::DecodeOne { task, datalake } => {
-            let task = task_decoder(task).unwrap();
-            let datalake = datalake_decoder(datalake).unwrap();
+            let task = task_decoder(task)?;
+            let datalake = datalake_decoder(datalake)?;
 
             println!("task: \n{:?}\n", task);
             println!("datalake: \n{:?}\n", datalake);
+            Ok(())
         }
         Commands::Run {
             tasks,
@@ -210,8 +226,6 @@ async fn main() {
             rpc_url,
             output_file,
             cairo_input,
-        } => {
-            handle_run(tasks, datalakes, rpc_url, output_file, cairo_input).await;
-        }
+        } => handle_run(tasks, datalakes, rpc_url, output_file, cairo_input).await,
     }
 }
