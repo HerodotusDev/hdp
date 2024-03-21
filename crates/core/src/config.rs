8140d675cbb264356355aa364df3804ab2287b4f
pia <76558220+rkdud007@users.noreply.github.com>
2024-03-21 20:17:36 +0900

    feat: Multichain Support (#47)
    * feat: chain id flag and provider
    
    * Chain id should be optional and rely on rpc_url
    
    * rpc_url, chain_id can be either pass .env , param

--- crates/core/src/config.rs
+++ crates/core/src/config.rs
@@ -7,6 +7,7 @@ pub struct Config {
     pub rpc_url: String,
     pub datalakes: String,
     pub tasks: String,
+    pub chain_id: u64,
 }
 
 impl Config {
@@ -14,10 +15,13 @@ impl Config {
         cli_rpc_url: Option<String>,
         cli_datalakes: Option<String>,
         cli_tasks: Option<String>,
+        cli_chain_id: Option<u64>,
     ) -> &'static Self {
         let rpc_url = cli_rpc_url.unwrap_or_else(|| env::var("RPC_URL").unwrap());
         let datalakes = cli_datalakes.unwrap_or_else(|| env::var("DATALAKES").unwrap());
         let tasks = cli_tasks.unwrap_or_else(|| env::var("TASKS").unwrap());
+        let chain_id =
+            cli_chain_id.unwrap_or_else(|| env::var("CHAIN_ID").unwrap().parse().unwrap());
 
         CONFIG
             .get_or_init(|| async {
@@ -25,6 +29,7 @@ impl Config {
                     rpc_url,
                     datalakes,
                     tasks,
+                    chain_id,
                 }
             })
             .await
