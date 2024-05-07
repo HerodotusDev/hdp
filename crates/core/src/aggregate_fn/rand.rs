a06099275833088858fb1b9355f1e049ad20e916
Pia <gayeongparkk@gmail.com>
2024-05-07 12:55:07 +0900

    rand for function context

--- crates/core/src/aggregate_fn/rand.rs
+++ crates/core/src/aggregate_fn/rand.rs
@@ -1,9 +1,10 @@
+use alloy_primitives::U256;
 use rand::{
     distributions::{Distribution, Standard},
     Rng,
 };
 
-use super::{integer::Operator, AggregationFunction};
+use super::{integer::Operator, AggregationFunction, FunctionContext};
 
 impl Distribution<AggregationFunction> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> AggregationFunction {
@@ -16,5 +17,14 @@ impl Distribution<Operator> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Operator {
         let index: u8 = rng.gen_range(1..=6);
         Operator::from_index(index).unwrap().unwrap()
+    }
+}
+
+impl Distribution<FunctionContext> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> FunctionContext {
+        FunctionContext {
+            operator: rng.sample(Standard),
+            value_to_compare: U256::from(rng.gen_range(0..=100)),
+        }
     }
 }
