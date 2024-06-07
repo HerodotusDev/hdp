a09b211f90ac2888116be292068d56e9a2709339
Pia <gayeongparkk@gmail.com>
2024-06-07 14:44:33 +0900

    wip preprocessor

--- crates/core-new/src/pre_processor/mod.rs
+++ crates/core-new/src/pre_processor/mod.rs
@@ -1,33 +1,53 @@
-use config::PreProcessorConfig;
+use std::vec;
 
-use crate::cairo_runner::pre_run::PreRunner;
+use crate::{cairo_runner::pre_run::PreRunner, input_generator::r#type::PreProcessorInput};
 use anyhow::Result;
-pub mod config;
 
 pub struct PreProcessResult {
+    /// Fetch points are the values that are required to run the module
     pub fetch_points: Vec<String>,
+    /// Module hash is the hash of the module that is being processed
     pub module_hash: String,
 }
 
 /*
-  Preprocessor is reponsible for identifying the required values
+  Preprocessor is reponsible for identifying the required values.
+  This will be most abstract layer of the preprocessor.
 */
 pub struct PreProcessor {
-    config: PreProcessorConfig,
+    pre_runner: PreRunner,
+}
+
+impl Default for PreProcessor {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl PreProcessor {
-    pub fn new(config: PreProcessorConfig) -> Self {
-        Self { config }
+    pub fn new() -> Self {
+        let pre_runner = PreRunner::new();
+        Self { pre_runner }
     }
 
-    pub fn process(&self) -> Result<PreProcessResult> {
-        let pre_runner = PreRunner::new();
-        let input_bytes = self.config.module_bytes();
-        let points = pre_runner.run(input_bytes.to_vec())?;
+    /// User request is pass as input of this function,
+    /// First it will generate input structure for preprocessor that need to pass to runner
+    /// Then it will run the preprocessor and return the result, fetch points
+    /// Fetch points are the values that are required to run the module
+    pub fn process(&self, module_hash: String, module_input: Vec<u8>) -> Result<PreProcessResult> {
+        let input = self.generate_input(module_hash, module_input);
+        let input_bytes = input.to_bytes();
+        let points = self.pre_runner.run(input_bytes.to_vec())?;
         Ok(PreProcessResult {
             fetch_points: points,
-            module_hash: self.config.module_hash().to_string(),
+            module_hash: input.module_hash.to_string(),
         })
+    }
+
+    /// Generate input structure for preprocessor that need to pass to runner
+    pub fn generate_input(&self, module_hash: String, module_input: Vec<u8>) -> PreProcessorInput {
+        // TODO: get module bytes from registry by module_hash
+        let module_bytes = vec![];
+        PreProcessorInput::new(module_hash, module_bytes, module_input)
     }
 }
