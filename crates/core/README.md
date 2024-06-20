0ff1edf4af4f336819c02ad103109bea136e605f
Pia <gayeongparkk@gmail.com>
2024-06-20 18:13:11 +0900

    update diagram, move module registry to `core`

---
+++ crates/core/README.md
@@ -0,0 +1,5 @@
+# Data Processor Core
+
+## Overview
+
+![](../../.github/core.png)
--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -3,4 +3,7 @@
 pub mod codec;
 pub mod compiler;
 pub mod config;
+pub mod constant;
+pub mod conversion;
 pub mod evaluator;
+pub mod module_registry;
