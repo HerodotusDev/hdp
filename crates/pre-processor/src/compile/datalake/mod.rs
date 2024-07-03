e0188002b614b3a7f994d7154b6a0162e68276ca
Pia <gayeongparkk@gmail.com>
2024-07-03 16:21:37 +0900

    datalake compile test case

--- crates/pre-processor/src/compile/datalake/mod.rs
+++ crates/pre-processor/src/compile/datalake/mod.rs
@@ -78,3 +78,156 @@ impl Compilable for DatalakeComputeVec {
         Ok(final_results)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use alloy::primitives::{address, B256, U256};
+    use hdp_primitives::{
+        aggregate_fn::AggregationFunction,
+        task::datalake::{
+            block_sampled::{
+                AccountField, BlockSampledCollection, BlockSampledDatalake, HeaderField,
+            },
+            compute::Computation,
+            transactions::{
+                IncludedTypes, TransactionField, TransactionReceiptField, TransactionsCollection,
+                TransactionsInBlockDatalake,
+            },
+        },
+    };
+    use hdp_provider::evm::provider::EvmProviderConfig;
+    use starknet::providers::Url;
+
+    use crate::compile::module::ModuleCompilerConfig;
+
+    use super::*;
+
+    const SEPOLIA_RPC_URL: &str =
+        "https://eth-sepolia.g.alchemy.com/v2/xar76cftwEtqTBWdF4ZFy9n8FLHAETDv";
+    const SN_SEPOLIA_RPC_URL: &str =
+        "https://starknet-sepolia.g.alchemy.com/v2/lINonYKIlp4NH9ZI6wvqJ4HeZj7T4Wm6";
+
+    #[tokio::test]
+    async fn test_compile_block_sampled_datalake_compute_vec() {
+        let program_path = "../../build/compiled_cairo/contract_dry_run.json";
+
+        let datalake_compute_vec = vec![
+            DatalakeCompute {
+                compute: Computation::new(AggregationFunction::MIN, None),
+                datalake: DatalakeEnvelope::BlockSampled(BlockSampledDatalake {
+                    block_range_start: 10001,
+                    block_range_end: 10005,
+                    increment: 1,
+                    sampled_property: BlockSampledCollection::Header(HeaderField::Number),
+                }),
+            },
+            DatalakeCompute {
+                compute: Computation::new(AggregationFunction::AVG, None),
+                datalake: DatalakeEnvelope::BlockSampled(BlockSampledDatalake {
+                    block_range_start: 6127485,
+                    block_range_end: 6127495,
+                    increment: 1,
+                    sampled_property: BlockSampledCollection::Account(
+                        address!("7f2c6f930306d3aa736b3a6c6a98f512f74036d4"),
+                        AccountField::Balance,
+                    ),
+                }),
+            },
+            DatalakeCompute {
+                compute: Computation::new(AggregationFunction::AVG, None),
+                datalake: DatalakeEnvelope::BlockSampled(BlockSampledDatalake {
+                    block_range_start: 6127485,
+                    block_range_end: 6127490,
+                    increment: 1,
+                    sampled_property: BlockSampledCollection::Storage(
+                        address!("75CeC1db9dCeb703200EAa6595f66885C962B920"),
+                        B256::from(U256::from(1)),
+                    ),
+                }),
+            },
+        ];
+
+        let module_config = ModuleCompilerConfig {
+            module_registry_rpc_url: Url::parse(SN_SEPOLIA_RPC_URL).unwrap(),
+            program_path: PathBuf::from(program_path),
+        };
+        let provider_config = EvmProviderConfig {
+            rpc_url: Url::parse(SEPOLIA_RPC_URL).unwrap(),
+            chain_id: 11155111,
+            max_requests: 100,
+        };
+        let compile_config = CompileConfig {
+            provider: provider_config,
+            module: module_config,
+        };
+        let results = datalake_compute_vec.compile(&compile_config).await.unwrap();
+        assert_eq!(results.headers.len(), 16);
+        assert_eq!(results.accounts.len(), 2);
+        // get first element
+        let account_proofs = results.accounts.iter().next().unwrap();
+        assert_eq!(account_proofs.proofs.len(), 11);
+        assert_eq!(results.storages.len(), 1);
+        let storage_proofs = results.storages.iter().next().unwrap();
+        assert_eq!(storage_proofs.proofs.len(), 6);
+        assert_eq!(results.transactions.len(), 0);
+        assert_eq!(results.transaction_receipts.len(), 0);
+        assert_eq!(results.mmr_meta.id, 27);
+    }
+
+    #[tokio::test]
+    async fn test_compile_transactions_datalake_compute_vec() {
+        let program_path = "../../build/compiled_cairo/contract_dry_run.json";
+
+        let datalake_compute_vec = vec![
+            DatalakeCompute {
+                compute: Computation::new(AggregationFunction::MIN, None),
+                datalake: DatalakeEnvelope::Transactions(TransactionsInBlockDatalake {
+                    target_block: 6127486,
+                    start_index: 0,
+                    end_index: 10,
+                    increment: 1,
+                    included_types: IncludedTypes::from(&[1, 1, 1, 1]),
+                    sampled_property: TransactionsCollection::Transactions(
+                        TransactionField::GasLimit,
+                    ),
+                }),
+            },
+            DatalakeCompute {
+                compute: Computation::new(AggregationFunction::MIN, None),
+                datalake: DatalakeEnvelope::Transactions(TransactionsInBlockDatalake {
+                    target_block: 6127485,
+                    start_index: 0,
+                    end_index: 11,
+                    increment: 1,
+                    included_types: IncludedTypes::from(&[1, 1, 1, 1]),
+                    sampled_property: TransactionsCollection::TranasactionReceipts(
+                        TransactionReceiptField::Success,
+                    ),
+                }),
+            },
+        ];
+
+        let module_config = ModuleCompilerConfig {
+            module_registry_rpc_url: Url::parse(SN_SEPOLIA_RPC_URL).unwrap(),
+            program_path: PathBuf::from(program_path),
+        };
+        let provider_config = EvmProviderConfig {
+            rpc_url: Url::parse(SEPOLIA_RPC_URL).unwrap(),
+            chain_id: 11155111,
+            max_requests: 100,
+        };
+        let compile_config = CompileConfig {
+            provider: provider_config,
+            module: module_config,
+        };
+        let results = datalake_compute_vec.compile(&compile_config).await.unwrap();
+        assert_eq!(results.headers.len(), 2);
+        assert_eq!(results.accounts.len(), 0);
+        assert_eq!(results.storages.len(), 0);
+        assert_eq!(results.transactions.len(), 10);
+        assert_eq!(results.transaction_receipts.len(), 11);
+        assert_eq!(results.mmr_meta.id, 27);
+    }
+}
