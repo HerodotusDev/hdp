28085a82c77cd64e666f1eeaeb311b8eec3294d3
Pia <gayeongparkk@gmail.com>
2024-07-18 17:50:02 +0200

    module finalize

--- crates/primitives/src/solidity_types/module.rs
+++ crates/primitives/src/solidity_types/module.rs
@@ -1,36 +1,11 @@
 use alloy::{
-    dyn_abi::{DynSolType, DynSolValue},
+    dyn_abi::DynSolValue,
     primitives::{keccak256, B256},
 };
-use anyhow::{bail, Result};
-use starknet_crypto::FieldElement;
 
 use crate::{task::module::Module, utils::felt_to_bytes32};
 
 impl Module {
-    pub fn encode(&self) -> Result<Vec<u8>> {
-        let task_code: DynSolValue = 1.into();
-        let class_hash: DynSolValue = DynSolValue::FixedBytes(felt_to_bytes32(self.class_hash), 32);
-        let module_inputs: DynSolValue = DynSolValue::FixedArray(
-            self.inputs
-                .iter()
-                .map(|input| DynSolValue::FixedBytes(felt_to_bytes32(*input), 32))
-                .collect(),
-        );
-        let input_length: DynSolValue = self.inputs.len().into();
-        // offset of class hash and task code
-        let offset: DynSolValue = (96).into();
-
-        let module_tuple_value = DynSolValue::Tuple(vec![
-            task_code,
-            class_hash,
-            offset,
-            input_length,
-            module_inputs,
-        ]);
-        Ok(module_tuple_value.abi_encode())
-    }
-
     pub fn commit(&self) -> B256 {
         let class_hash: DynSolValue = DynSolValue::FixedBytes(felt_to_bytes32(self.class_hash), 32);
 
@@ -46,35 +21,6 @@ impl Module {
         let module_tuple_value =
             DynSolValue::Tuple(vec![class_hash, offset, input_length, module_inputs]);
         keccak256(module_tuple_value.abi_encode())
-    }
-
-    pub fn decode(encoded: &[u8]) -> Result<Self> {
-        let abi_type: DynSolType = "(uint256,bytes32,bytes32[])".parse()?;
-        let decoded = abi_type.abi_decode_sequence(encoded)?;
-        let value = decoded.as_tuple().unwrap();
-        let task_code = value[0].as_uint().unwrap().0.to_string().parse::<u8>()?;
-
-        if task_code != 1 {
-            bail!("Invalid task code");
-        }
-
-        let class_hash =
-            FieldElement::from_byte_slice_be(value[1].as_fixed_bytes().unwrap().0).unwrap();
-        let module_inputs: Vec<FieldElement> = value[2]
-            .as_array()
-            .unwrap()
-            .iter()
-            .map(|input| {
-                FieldElement::from_byte_slice_be(input.as_fixed_bytes().unwrap().0).unwrap()
-            })
-            .collect();
-
-        let module = Module {
-            class_hash,
-            inputs: module_inputs,
-            local_class_path: None,
-        };
-        Ok(module)
     }
 }
 
@@ -99,7 +45,6 @@ mod tests {
             ],
             local_class_path: None,
         };
-        let encoded = module.encode().unwrap();
 
         let expected_commit = module.commit();
         assert_eq!(
@@ -107,8 +52,5 @@ mod tests {
             B256::from_str("0x96ed7a050dc775d5a3091181336837f6d807286150878e16d66df377df2fe89a")
                 .unwrap()
         );
-
-        let decoded = Module::decode(&encoded).unwrap();
-        assert_eq!(decoded, module);
     }
 }
