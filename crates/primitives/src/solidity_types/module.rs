9ea14c748f60bfe7a11ac173da5a64cdbd1ce78e
Pia <gayeongparkk@gmail.com>
2024-07-18 14:49:49 +0200

    decode

--- crates/primitives/src/solidity_types/module.rs
+++ crates/primitives/src/solidity_types/module.rs
@@ -1,8 +1,9 @@
 use alloy::{
-    dyn_abi::DynSolValue,
+    dyn_abi::{DynSolType, DynSolValue},
     primitives::{keccak256, B256},
 };
-use anyhow::Result;
+use anyhow::{bail, Result};
+use starknet_crypto::FieldElement;
 
 use crate::{task::module::Module, utils::felt_to_bytes32};
 
@@ -16,8 +17,17 @@ impl Module {
                 .map(|input| DynSolValue::FixedBytes(felt_to_bytes32(*input), 32))
                 .collect(),
         );
+        let input_length: DynSolValue = self.inputs.len().into();
+        // offset of class hash and task code
+        let offset: DynSolValue = (96).into();
 
-        let module_tuple_value = DynSolValue::Tuple(vec![task_code, class_hash, module_inputs]);
+        let module_tuple_value = DynSolValue::Tuple(vec![
+            task_code,
+            class_hash,
+            offset,
+            input_length,
+            module_inputs,
+        ]);
         Ok(module_tuple_value.abi_encode())
     }
 
@@ -37,14 +47,41 @@ impl Module {
             DynSolValue::Tuple(vec![class_hash, offset, input_length, module_inputs]);
         keccak256(module_tuple_value.abi_encode())
     }
+
+    pub fn decode(encoded: &[u8]) -> Result<Self> {
+        let abi_type: DynSolType = "(uint256,bytes32,bytes32[])".parse()?;
+        let decoded = abi_type.abi_decode_sequence(encoded)?;
+        let value = decoded.as_tuple().unwrap();
+        let task_code = value[0].as_uint().unwrap().0.to_string().parse::<u8>()?;
+
+        if task_code != 1 {
+            bail!("Invalid task code");
+        }
+
+        let class_hash =
+            FieldElement::from_byte_slice_be(value[1].as_fixed_bytes().unwrap().0).unwrap();
+        let module_inputs: Vec<FieldElement> = value[2]
+            .as_array()
+            .unwrap()
+            .iter()
+            .map(|input| {
+                FieldElement::from_byte_slice_be(input.as_fixed_bytes().unwrap().0).unwrap()
+            })
+            .collect();
+
+        let module = Module {
+            class_hash,
+            inputs: module_inputs,
+            local_class_path: None,
+        };
+        Ok(module)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
-
-    use alloy::hex;
     use starknet_crypto::FieldElement;
+    use std::str::FromStr;
 
     use super::*;
 
@@ -63,14 +100,15 @@ mod tests {
             local_class_path: None,
         };
         let encoded = module.encode().unwrap();
-        let expected = hex::encode(encoded);
-        println!("{}", expected);
 
         let expected_commit = module.commit();
         assert_eq!(
             expected_commit,
             B256::from_str("0x96ed7a050dc775d5a3091181336837f6d807286150878e16d66df377df2fe89a")
                 .unwrap()
-        )
+        );
+
+        let decoded = Module::decode(&encoded).unwrap();
+        assert_eq!(decoded, module);
     }
 }
