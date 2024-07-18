9ee21ec208ed0e452e70e95b856d9c95a40fe4ab
Pia <gayeongparkk@gmail.com>
2024-07-18 13:26:30 +0200

    module commit

--- crates/primitives/src/solidity_types/module.rs
+++ crates/primitives/src/solidity_types/module.rs
@@ -23,20 +23,26 @@ impl Module {
 
     pub fn commit(&self) -> B256 {
         let class_hash: DynSolValue = DynSolValue::FixedBytes(felt_to_bytes32(self.class_hash), 32);
+
         let module_inputs: DynSolValue = DynSolValue::FixedArray(
             self.inputs
                 .iter()
                 .map(|input| DynSolValue::FixedBytes(felt_to_bytes32(*input), 32))
                 .collect(),
         );
-
-        let module_tuple_value = DynSolValue::Tuple(vec![class_hash, module_inputs]);
+        let input_length: DynSolValue = self.inputs.len().into();
+        // offset of class hash
+        let offset: DynSolValue = (64).into();
+        let module_tuple_value =
+            DynSolValue::Tuple(vec![class_hash, offset, input_length, module_inputs]);
         keccak256(module_tuple_value.abi_encode())
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
     use alloy::hex;
     use starknet_crypto::FieldElement;
 
@@ -61,6 +67,10 @@ mod tests {
         println!("{}", expected);
 
         let expected_commit = module.commit();
-        println!("{}", expected_commit);
+        assert_eq!(
+            expected_commit,
+            B256::from_str("0x96ed7a050dc775d5a3091181336837f6d807286150878e16d66df377df2fe89a")
+                .unwrap()
+        )
     }
 }
