70b79fb2bb8c2c3d969732ddc8c10f097ab7a478
Pia <gayeongparkk@gmail.com>
2024-07-25 10:25:03 +0200

    feat: default should follow local compatible

--- crates/primitives/src/constant.rs
+++ crates/primitives/src/constant.rs
@@ -3,8 +3,8 @@ use lazy_static::lazy_static;
 
 pub const DRY_CAIRO_RUN_OUTPUT_FILE: &str = "dry_run_output.json";
 pub const SOUND_CAIRO_RUN_OUTPUT_FILE: &str = "cairo_run_output.json";
-pub const DEFAULT_DRY_CAIRO_RUN_CAIRO_FILE: &str = "/hdp-cairo/build/contract_dry_run.json";
-pub const DEFAULT_SOUND_CAIRO_RUN_CAIRO_FILE: &str = "/hdp-cairo/build/hdp.json";
+pub const DEFAULT_DRY_CAIRO_RUN_CAIRO_FILE: &str = "build/contract_dry_run.json";
+pub const DEFAULT_SOUND_CAIRO_RUN_CAIRO_FILE: &str = "build/hdp.json";
 
 lazy_static! {
     pub static ref NEW_EXAMPLE_CONTRACT: CasmContractClass = read_compiled_class_artifact(
