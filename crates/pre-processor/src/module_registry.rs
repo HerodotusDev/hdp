498c0b0954fbe69b9c8316395c06e8753024a315
Pia <gayeongparkk@gmail.com>
2024-07-30 12:14:54 -0400

    hot fix: path update

--- crates/pre-processor/src/module_registry.rs
+++ crates/pre-processor/src/module_registry.rs
@@ -141,7 +141,7 @@ impl ModuleRegistry {
         let program_hash_key = program_hash.to_string();
         let branch = "v2-fix";
         let api_url = format!(
-            "https://api.github.com/repos/HerodotusDev/hdp/contents/crates/preprocessor/module-registery/{}.json?ref={}",
+            "https://api.github.com/repos/HerodotusDev/hdp/contents/crates/pre-processor/module-registery/{}.json?ref={}",
             program_hash_key, branch
         );
 
