e2d90b8963bb17a50ed268075b6c20dbb9448306
lakewik <wikjezioro@op.pl>
2024-12-12 14:29:42 +0100

    Change indexer URL

--- hdp/src/constant.rs
+++ hdp/src/constant.rs
@@ -7,6 +7,6 @@ pub const DEFAULT_SOUND_CAIRO_RUN_CAIRO_FILE: &str = "build/hdp.json";
 pub const HERODOTUS_RS_INDEXER_URL: &str =
     "https://rs-indexer.api.herodotus.cloud/accumulators/proofs";
 pub const HERODOTUS_RS_INDEXER_STAGING_URL: &str =
-    "https://staging.rs-indexer.api.herodotus.cloud/accumulators/proofs";
+    "https://grower-indexer.api.herodotus.cloud/accumulators/proofs";
 pub const HERODOTUS_PROGRAM_REGISTRY_URL: &str =
     "http://program-registery.api.herodotus.cloud/get-program?program_hash";
